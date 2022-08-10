2ea8f6dbd46f80db94eff99393683dcf6fda1e25
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-10 18:36:03 +0300

    update on sign in screen header

--- class-sys/App.js
+++ class-sys/App.js
@@ -11,8 +11,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Welcome' component={Welcome} options={{headerShown: false}}/>
-        <Stack.Screen name='SignIn' component={SignIn} />
+        <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }} />
+
+        <Stack.Screen name='SignIn' component={SignIn} options={{
+          title: 'Sign In', headerStyle: {
+            backgroundColor: '#020742',
+            elevation: 0, // remove shadow on Android
+            shadowOpacity: 0, // remove shadow on iOS
+            borderBottomWidth: 0,
+          },
+          headerTintColor: '#2196F3',
+        }} />
+
         <Stack.Screen name='SignUp' component={SignUp} />
         <Stack.Screen name='Dashboard' component={Dashboard} />
       </Stack.Navigator>
