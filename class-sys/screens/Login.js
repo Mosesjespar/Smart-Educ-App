1311cc4b81864e41f557c8822c06e0226b8efa98
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-12 17:31:42 +0300

    Update Login.js

--- class-sys/screens/Login.js
+++ class-sys/screens/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Image, View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
-import UserLogin from "../ApiServices/UserLogin";
+import { Image, View, Text, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView, TextInput } from "react-native";
+import { Button } from "react-native-paper";
+
+
 export const base_url = 'http://127.0.0.1:5000'
 
 export default function SignIn({ navigation }) {
-    const [data, setData] = React.useState('n')
     const [UserName, setUserName] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [token, setToken] = React.useState('')
@@ -13,11 +14,7 @@ export default function SignIn({ navigation }) {
         username: UserName,
         passw: password
     }
-    React.useEffect(() => {
-        fetch(base_url + '/data'
-        ).then(data => data.json()).then(data => { setData(data.age) }).catch(e => console.log('an error: ', e))
 
-    }, [])
     const UserLogin = async (info) => {
         let opts = {
             method: "POST",
@@ -29,66 +26,77 @@ export default function SignIn({ navigation }) {
             },
 
         }
-        fetch(base_url + '/login', opts
-        ).then(response => {
-            if (response.status === 200) {
-                navigation.navigate('Dashboard', { name: UserName })
-                setResultMsg('')
-                return (response.json())
+        if (navigator.onLine) {
+            fetch(base_url + '/login', opts
+            ).then(response => {
+                if (response.status === 200) {
+                    navigation.navigate('Dashboard', { name: UserName })
+                    setResultMsg('')
+                    return (response.json())
+                }
+                else {
+                    setResultMsg('Invalid Username or password')
+                    setPassword('')
+                    setUserName('')
+                    return (response.json())
+                }
             }
-            else {
-                setResultMsg('Invalid Username or password')
-                setPassword('')
-                setUserName('')
-                return (response.json())
+            ).then(data => {
+                const tkn = sessionStorage.setItem('token', data.access_token)
+                console.log(data)
+                setToken(tkn)
             }
-        }
-        ).then(data => {
-            const tkn = sessionStorage.setItem('token', data.access_token)
-            console.log(data)
-            setToken(tkn)
-        }
 
-        ).catch(e => console.log('error:', e))
+            ).catch(e => console.log('error:', e))
+        }
+        else{
+            setResultMsg('Unable to Connect to the Internet')
+        }
     }
     return (
         <SafeAreaView>
             <ScrollView>
-                <View style={{ padding: '5vmin', backgroundColor: 'white', height: '100%' }}>
-                    <View style={styles.container}>
-                        <Text style={styles.stxt}>{data}</Text>
-                        <Image source={require('../images/smart-edu-nbg.png')} style={styles.img} />
+                <View style={styles.container}>
+                    <Image source={require('../images/logo.png')} style={styles.img} />
 
-                        <TextInput style={styles.input} placeholder='User Name'
-                            value={UserName}
-                            onChangeText={(text) => setUserName(text)} />
+                    <TextInput
+                        placeholder="User Name"
+                        placeholderTextColor={'#2196F3'}
+                        value={UserName}
+                        onChangeText={text => setUserName(text)}
+                        style={styles.input}
 
-                        <TextInput style={styles.input} placeholder='Password' secureTextEntry value={password}
-                            onChangeText={(text) => setPassword(text)} />
+                    />
 
-                        <TouchableOpacity onPress={() => UserLogin(userInfo)}>
-                            <View style={styles.btn2}>
-                                <Text style={styles.btn2t}>SIGN IN</Text>
-                            </View>
+                    <TextInput style={styles.input} placeholder='Password' secureTextEntry value={password}
+                        onChangeText={(text) => setPassword(text)}
+                        placeholderTextColor={'#2196F3'}
+                    />
+                    <Button mode="contained" onPress={() => UserLogin(userInfo)}
+                        color={'#2196F3'}
+                        labelStyle={{ color: "white", fontSize: 16, letterSpacing: 2, fontWeight: 500 }}
+                        contentStyle={{ width: '80vw', paddingTop: 5 }}
+                    >
+                        SIGN IN
+                    </Button>
+
+                    <Text style={{
+                        color: 'red',
+                        fontWeight: 600,
+                        fontSize: '4vmin',
+                        marginBottom: 2
+                    }}>{resultMsg}</Text>
+
+                    <View style={styles.reg}>
+                        <Text style={{ fontSize: '4.2vmin', color: 'white' }}>Don't Have an Account Yet? </Text>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('SignUp')}
+                        >
+                            <Text style={styles.regLink}> Register</Text>
                         </TouchableOpacity>
-
-                        <Text style={{
-                            color: 'red',
-                            fontWeight: 600,
-                            fontSize: '4vmin',
-                            paddingTop: 5
-                        }}>{resultMsg}</Text>
-
-                        <View style={styles.reg}>
-                            <Text style={{ fontSize: '4.2vmin' }}>Don't Have an Account Yet?</Text>
-                            <TouchableOpacity
-                                onPress={() => navigation.navigate('SignUp')}
-                            >
-                                <Text style={styles.regLink}> Register</Text>
-                            </TouchableOpacity>
-                        </View>
                     </View>
                 </View>
+
             </ScrollView>
         </SafeAreaView>
     )
@@ -97,61 +105,20 @@ export default function SignIn({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: '100%',
-        borderWidth: 1,
-        borderRadius: 3,
-        borderColor: '#ddd',
-        textAlign: 'center',
+        width: '100%',
+        minHeight: '100%',
+        alignItems: 'center',
         justifyContent: 'center',
-        shadowOffset: { width: 0, height: 1.3 },
-        shadowOpacity: 0.1,
-        paddingBottom: '20vmin',
-        backgroundColor: 'white'
+        // paddingTop: '20vmin',
+        marginBottom: '20vmin',
+        backgroundColor: '#020742',
 
-    },
-    stxt: {
-        color: '#41aafa',
-        fontWeight: 600,
-        fontSize: '4.6vmin',
-        paddingTop: 5
     },
     img: {
-        width: '35vw',
-        height: '35vh',
+        width: '48vw',
+        height: '48vh',
+        marginBottom: '2vmin',
         alignSelf: 'center',
-    },
-    input: {
-        fontSize: '4.6vmin',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        width: '80vw',
-        alignSelf: 'center',
-        marginTop: 5,
-        fontSize: '4vmin',
-        outlineStyle: 'none',
-        height: '9vmin',
-        paddingLeft: 10,
-        backgroundColor: '#ddd'
-    },
-    btn2: {
-        textAlign: 'center',
-        marginTop: 5,
-        justifyContent: 'center',
-        flex: 1
-
-    },
-    btn2t: {
-        fontWeight: 500,
-        padding: 7,
-        width: '80vw',
-        fontSize: '4.6vmin',
-        backgroundColor: '#2196F3',
-        color: 'white',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        letterSpacing: 3,
-        alignSelf: 'center',
-
     },
     reg: {
         flex: 1,
@@ -165,5 +132,20 @@ const styles = StyleSheet.create({
         color: '#2196F3',
         fontSize: '4.2vmin',
         fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: 'transparent',
+        borderColor: '#2196F3',
+        borderWidth: 1,
+        color: '#2196F3',
+        borderRadius: 3,
+        fontSize: 16,
+        letterSpacing: 1.1,
+        fontWeight: 400,
+        width: '80vw',
+        margin: 5,
+        height: 40,
+        outlineStyle: 'none',
+        padding: 10
     }
 })
