866a5f698e6f216453813d35cd034e7178153d68
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-31 16:41:26 +0300

    Update server.py

--- class-sys/backend/server.py
+++ class-sys/backend/server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, json
 from flask_cors import CORS
+from Database import Student, Teacher
 
 api = Flask(__name__)
 CORS(api)
@@ -8,8 +9,19 @@ CORS(api)
 @api.route('/')
 def main():
     return {
-        'msg': 'hello world'
+        'msg': 'Hello There, Welcome to Study Life'
     }
+
+
+@api.route('/register', methods=['POST'])
+def Register_Student():
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    pin = request.json['pin']
+    Class = request.json['Class']
+    gender = request.json['gender']
+    new_student = Student(first_name, last_name, Class, pin, gender)
+    return new_student.register()
 
 
 if __name__ == '__main__':
