3fc78417d46a9af1709942b6ab34aa87b762706f
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-31 16:41:04 +0300

    update on student registration endpoint

--- class-sys/backend/Database.py
+++ class-sys/backend/Database.py
@@ -12,20 +12,43 @@ class Student:
     def register(self):
         with sqlite3.connect('school.db') as conn:
             cur = conn.cursor()
+
             reg_query = '''INSERT INTO students(First_Name,Last_Name,Class,Pin,Gender)
                 VALUES (?,?,?,?,?)
                 '''
             detail_query = 'SELECT * FROM students WHERE First_Name = ? AND Last_Name = ?'
+
             reg_data = (self.first_name, self.last_name, self.clas, self.pin, self.gender)
+
             detail_data = (self.first_name, self.last_name)
+
             cur.execute(detail_query, detail_data)
+
             details = cur.fetchall()
+
             if len(details) == 0:
                 cur.execute(reg_query, reg_data)
                 conn.commit()
                 print(f'{self.first_name} {self.last_name} is registered')
+
+                response = {
+                    'message': f'{self.first_name} {self.last_name} is registered',
+                    'First Name': self.first_name,
+                    'Last Name': self.last_name,
+                    'Class': self.clas,
+                    'pin': self.pin,
+                    'gender': self.gender
+                }
+                return response
+
             else:
                 print(f'{self.first_name} {self.last_name} is already a student')
+
+                response = {
+                    'message': f'{self.first_name} {self.last_name} is already a student',
+                }
+
+                return response
 
     def details(self):
         with sqlite3.connect('school.db') as conn:
@@ -33,7 +56,15 @@ class Student:
             detail_query = 'SELECT * FROM students WHERE First_Name = ? AND Last_Name = ?'
             data = (self.first_name, self.last_name)
             cur.execute(detail_query, data)
-            return cur.fetchall()
+            results = list(cur.fetchall()[0])
+            info = {
+                'first_name': results[0],
+                'last_name': results[1],
+                'class': results[2],
+                'pin': results[3],
+                'gender': results[4]
+            }
+            return info
 
     def marks(self):
         pass
@@ -49,3 +80,36 @@ class Teacher:
         self.classes = classes
         self.pin = pin
         self.gender = gender
+        self.subjects = subjects
+
+    def register(self):
+        with sqlite3.connect('school.db') as conn:
+            cur = conn.cursor()
+            reg_query = '''INSERT INTO teachers(First_Name,Last_Name,Class,Pin,Gender,Subjects)
+                VALUES (?,?,?,?,?,?)
+                '''
+            detail_query = 'SELECT * FROM teachers WHERE First_Name = ? AND Last_Name = ?'
+            reg_data = (self.first_name, self.last_name, self.classes, self.pin, self.gender, self.subjects)
+            detail_data = (self.first_name, self.last_name)
+            cur.execute(detail_query, detail_data)
+            details = cur.fetchall()
+            if len(details) == 0:
+                cur.execute(reg_query, reg_data)
+                conn.commit()
+                print(f'{self.first_name} {self.last_name} is registered')
+                response = {
+                    'message': f'{self.first_name} {self.last_name} is registered',
+                    'First Name': self.first_name,
+                    'Last Name': self.last_name,
+                    'Class': self.classes,
+                    'pin': self.pin,
+                    'gender': self.gender,
+                    'subjects': self.subjects
+                }
+                return response
+            else:
+                print(f'{self.first_name} {self.last_name} is already a Teacher')
+                response = {
+                    'message': f'{self.first_name} {self.last_name} is already a Teacher',
+                }
+                return response
