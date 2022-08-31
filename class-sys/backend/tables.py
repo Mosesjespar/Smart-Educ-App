f0b8604b82742852cd0c30bd41716ab318fcf61d
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-31 16:41:37 +0300

    Create tables.py

---
+++ class-sys/backend/tables.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+# created
+def Students_table():
+    conn = sqlite3.connect('school.db')
+    cur = conn.cursor()
+    query = '''CREATE TABLE students(
+                First_Name VARCHAR NOT NULL,
+                Last_Name VARCHAR NOT NULL,
+                Class VARCHAR NOT NULL,
+                Pin INT NOT NULL,
+                Gender VARCHAR(1) NOT NULL
+   )'''
+    cur.execute(query)
+    print('students table created')
+
+
+# created
+def Teachers_table():
+    conn = sqlite3.connect('school.db')
+    cur = conn.cursor()
+    query = '''CREATE TABLE teachers(
+                First_Name VARCHAR NOT NULL,
+                Last_Name VARCHAR NOT NULL,
+                Class VARCHAR NOT NULL,
+                Pin INT NOT NULL,
+                Gender VARCHAR(1) NOT NULL,
+                Subjects VARCHAR NOT NULL
+    )'''
+    cur.execute(query)
+    print('teachers table created')
+
+
+
