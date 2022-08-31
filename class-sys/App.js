57673fd6fc726bee89b8e69543bf135ab71a04f6
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-08-31 16:40:22 +0300

    update on the navigation screens

--- class-sys/App.js
+++ class-sys/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Welcome from './screens/Welcome'
-import SignIn from './screens/Login';
-import SignUp from './screens/SignUp';
+import {StudentLogin} from './screens/Login';
 import StudentOpts from './screens/StudentOpts';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StudentSignUp from './screens/StudentSignUp'
 import TeacherOpts from './screens/TeacherOpts';
-
+import TeacherSignUp from './screens/TeacherSignUp';
 
 const Stack = createNativeStackNavigator()
 
@@ -26,24 +25,9 @@ export default function App() {
 
         <Stack.Screen name='teacheropts' component={TeacherOpts} options={{ headerShown: false }} />
 
-        <Stack.Screen name='SignIn' component={SignIn} options={{
-          title: 'Sign In',
-          headerStyle: { backgroundColor: '#020742', elevation: 0, shadowOpacity: 0, borderBottomWidth: 0, }, headerTintColor: '#2196F3',
-        }} />
+        <Stack.Screen name='studentlogin' component={StudentLogin} options={{ headerShown: false }} />
 
-        <Stack.Screen name='SignUp' component={SignUp} options={{
-          headerShown: false,
-          title: 'Sign Up',
-          headerStyle: {
-            backgroundColor: '#020742',
-            elevation: 0,
-            shadowOpacity: 0,
-            borderBottomWidth: 0,
-          },
-          headerTintColor: '#2196F3',
-        }}
-
-        />
+        <Stack.Screen name='teacherSignUp' component={TeacherSignUp} options={{ headerShown: false }} />
 
       </Stack.Navigator>
     </NavigationContainer>
