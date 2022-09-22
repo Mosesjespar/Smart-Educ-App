73ca5ce240f00be06c20d7a4ca3da4396289c3b8
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-09-22 09:20:10 +0300

    Update Database.py

--- class-sys/backend/Database.py
+++ class-sys/backend/Database.py
@@ -9,6 +9,15 @@ class Student:
         self.pin = pin
         self.gender = gender
 
+    def isRegistered(self):
+        with sqlite3.connect('school.db') as conn:
+            cur = conn.cursor()
+            detail_query = 'SELECT * FROM students WHERE First_Name = ? AND Last_Name = ?'
+            detail_data = (self.first_name, self.last_name)
+            cur.execute(detail_query, detail_data)
+            details = cur.fetchall()
+        return len(details) != 0
+
     def register(self):
         with sqlite3.connect('school.db') as conn:
             cur = conn.cursor()
@@ -16,39 +25,21 @@ class Student:
             reg_query = '''INSERT INTO students(First_Name,Last_Name,Class,Pin,Gender)
                 VALUES (?,?,?,?,?)
                 '''
-            detail_query = 'SELECT * FROM students WHERE First_Name = ? AND Last_Name = ?'
-
             reg_data = (self.first_name, self.last_name, self.clas, self.pin, self.gender)
 
-            detail_data = (self.first_name, self.last_name)
+            cur.execute(reg_query, reg_data)
+            conn.commit()
+            print(f'{self.first_name} {self.last_name} is registered')
 
-            cur.execute(detail_query, detail_data)
-
-            details = cur.fetchall()
-
-            if len(details) == 0:
-                cur.execute(reg_query, reg_data)
-                conn.commit()
-                print(f'{self.first_name} {self.last_name} is registered')
-
-                response = {
-                    'message': f'{self.first_name} {self.last_name} is registered',
-                    'First Name': self.first_name,
-                    'Last Name': self.last_name,
-                    'Class': self.clas,
-                    'pin': self.pin,
-                    'gender': self.gender
-                }
-                return response
-
-            else:
-                print(f'{self.first_name} {self.last_name} is already a student')
-
-                response = {
-                    'message': f'{self.first_name} {self.last_name} is already a student',
-                }
-
-                return response
+            response = {
+                'isRegistered': 'YES',
+                'First Name': self.first_name,
+                'Last Name': self.last_name,
+                'Class': self.clas,
+                'pin': self.pin,
+                'gender': self.gender
+            }
+            return response
 
     def details(self):
         with sqlite3.connect('school.db') as conn:
@@ -64,13 +55,23 @@ class Student:
                 'pin': results[3],
                 'gender': results[4]
             }
-            return info
-
-    def marks(self):
-        pass
+            if len(results) == 0:
+                return {
+                    'msg': f'{self.first_name} {self.last_name} is not a Student'
+                }
+            else:
+                return info
 
     def class_teacher(self):
-        pass
+        with sqlite3.connect('school.db') as conn:
+            cur = conn.cursor()
+            detail_query = 'SELECT * FROM classTeachers WHERE Class = ?'
+            data = self.clas
+            cur.execute(detail_query, data)
+            results = list(cur.fetchall()[0])
+            return {
+                'tr': results[0]
+            }
 
 
 class Teacher:
@@ -82,34 +83,32 @@ class Teacher:
         self.gender = gender
         self.subjects = subjects
 
+    def isRegistered(self):
+        with sqlite3.connect('school.db') as conn:
+            cur = conn.cursor()
+            detail_query = 'SELECT * FROM teachers WHERE First_Name = ? AND Last_Name = ?'
+            detail_data = (self.first_name, self.last_name)
+            cur.execute(detail_query, detail_data)
+            details = cur.fetchall()
+        return len(details) != 0
+
     def register(self):
         with sqlite3.connect('school.db') as conn:
             cur = conn.cursor()
             reg_query = '''INSERT INTO teachers(First_Name,Last_Name,Class,Pin,Gender,Subjects)
                 VALUES (?,?,?,?,?,?)
                 '''
-            detail_query = 'SELECT * FROM teachers WHERE First_Name = ? AND Last_Name = ?'
             reg_data = (self.first_name, self.last_name, self.classes, self.pin, self.gender, self.subjects)
-            detail_data = (self.first_name, self.last_name)
-            cur.execute(detail_query, detail_data)
-            details = cur.fetchall()
-            if len(details) == 0:
-                cur.execute(reg_query, reg_data)
-                conn.commit()
-                print(f'{self.first_name} {self.last_name} is registered')
-                response = {
-                    'message': f'{self.first_name} {self.last_name} is registered',
-                    'First Name': self.first_name,
-                    'Last Name': self.last_name,
-                    'Class': self.classes,
-                    'pin': self.pin,
-                    'gender': self.gender,
-                    'subjects': self.subjects
-                }
-                return response
-            else:
-                print(f'{self.first_name} {self.last_name} is already a Teacher')
-                response = {
-                    'message': f'{self.first_name} {self.last_name} is already a Teacher',
-                }
-                return response
+            cur.execute(reg_query, reg_data)
+            conn.commit()
+            print(f'{self.first_name} {self.last_name} is registered')
+            response = {
+                'message': f'{self.first_name} {self.last_name} is registered',
+                'First Name': self.first_name,
+                'Last Name': self.last_name,
+                'Class': self.classes,
+                'pin': self.pin,
+                'gender': self.gender,
+                'subjects': self.subjects
+            }
+            return response
