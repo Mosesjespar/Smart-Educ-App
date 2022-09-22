d8763b493be123604a46e4ccbc5935b814e16090
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-09-22 09:19:36 +0300

    Update server.py

--- class-sys/backend/server.py
+++ class-sys/backend/server.py
@@ -21,7 +21,14 @@ def Register_Student():
     Class = request.json['Class']
     gender = request.json['gender']
     new_student = Student(first_name, last_name, Class, pin, gender)
-    return new_student.register()
+    return new_student.register() if not new_student.isRegistered() else {'isRegistered': True}
+
+
+@api.route('/student-login', methods=['POST'])
+def Student_Login():
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    pin = request.json['pin']
 
 
 if __name__ == '__main__':
